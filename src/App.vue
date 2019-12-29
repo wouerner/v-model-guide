3c24bf0b6037ba08146335a8070f61eaf97aee59
wouerner <wouerner@protonmail.com>
2019-12-29 13:07:55 -0300

    router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <Teste/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Teste from './components/Teste.vue'
-
-export default {
-  name: 'app',
-  components: {
-   Teste 
-  }
-}
-</script>
-
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import VueAnalytics from 'vue-analytics'
+import router from './router'
 
 Vue.use(VueAnalytics, {
-  id: 'UA-155066120-1'
+    id: 'UA-155066120-1',
+    router
 })
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
